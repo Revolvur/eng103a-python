2ecf9ec075c210de5018b9a81078c66a65dbabc6
Yacob <gfranco@hotmail.co.uk>
2022-01-24 11:25:26 +0000

    added class values indicator

--- 01_Variables_Intro/02_Input.py
+++ 01_Variables_Intro/02_Input.py
@@ -2,8 +2,10 @@ name = input("What is your name?\n")
 print("Hello " + name + ', you are very good looking.')
 age = int(input("Also, how old are you?\n"))
 print("You are " + str(age) + " years old.")
+print("Class age is ", type(age))
 age_dad = int(input("Lastly, how old is your dad?\n"))
 age_diff = (age_dad - age)
+print("Class is age_diff is ", type(age_diff))
 print("The age difference between you and your dad is by " + str(age_diff) + " years.")
 
 # Ask 3 questions
