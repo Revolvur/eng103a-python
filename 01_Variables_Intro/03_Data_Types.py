6bafb2145718123f7b695d13ea63086b2754fc7e
Yacob <gfranco@hotmail.co.uk>
2022-01-24 12:06:55 +0000

    Added methods etc

--- 01_Variables_Intro/03_Data_Types.py
+++ 01_Variables_Intro/03_Data_Types.py
@@ -1,24 +1,67 @@
-i = 375  # int
-f = 3.75  # float
-c = 3j + 2  # complex
+# Numeric!
 
-print(c, type(c))
+# control + / to comment all or uncomment
+#
+# i = 375  # int
+# f = 3.75  # float
+# c = 3j + 2  # complex
+#
+# print(c, type(c))
+#
+# add = 3 + 5
+# subtract = 3 - 5
+# multiply = 3 * 5
+# divide = 3 / 5
+# power = 3 ** 5  # three to the power of 5
+# modulo = 3 % 5  # gives remainder after division
+#
+# print(13 % 3)  # 4 * 3 = 12, 13 - 12 = 1
+# print(12 % 3)  # = 0
+#
+# print(13 / 3)  # 4.33333333333333
+# print(13 // 3, 13 % 3)  # Division to a whole number = 4 to the lowest number with remainder 1
+#
+# third = 1 / 3
+# print(third)
+# print(third * 3)  # float
+#
+# print(25 ** 0.5)  # square root
 
-add = 3 + 5
-subtract = 3 - 5
-multiply = 3 * 5
-divide = 3 / 5
-power = 3 ** 5  # three to the power of 5
-modulo = 3 % 5  # gives remainder after division
+# Strings!
 
-print(13 % 3)  # 4 * 3 = 12, 13 - 12 = 1
-print(12 % 3)  # = 0
+# single = 'hi'
+# double = "yo"
+# # mix = 'String in both"  will not work as you can't mix and match
+#
+# # failure = 'This is David's string'
+#
+# single_in_double = "This is David's string"
+# double_in_single = 'This is a "string"'
+# both = "This is David's \"string\""
+# print(both)
 
-print(13 / 3)  # 4.33333333333333
-print(13 // 3, 13 % 3)  # Division to a whole number = 4 to the lowest number with remainder 1
+# Indexing and slicing
 
-third = 1 / 3
-print(third)
-print(third * 3)  # float
+# hi = "Hello World!"
+# print(hi[0])  # Python starts counting at zero!
+# print(hi[6])
+# print(hi[-1])
+# print(hi[0:7])  # Hello, everything up to but not including 7
+# print(hi[3:8])  # lo Wo
+#
+# print(len(hi))  # length of string
 
-print(25 ** 0.5)  # square root
+# Methods
+
+white_space = "     lots of white space      "
+print(len(white_space))
+print(white_space.strip())  # .methods
+print(white_space.lstrip())  # .methods l is from left and r from right
+white_space2 = "......lots of white space......"
+print(white_space2.strip("."))
+print(white_space.upper())
+print(white_space.strip().capitalize())
+
+
+
+
