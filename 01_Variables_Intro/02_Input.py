be457df2d07f2993ec5b35c70b723432e65721f2
Yacob <gfranco@hotmail.co.uk>
2022-01-24 11:11:26 +0000

    Added input questions

---
+++ 01_Variables_Intro/02_Input.py
@@ -0,0 +1,11 @@
+name = input("What is your name?\n")
+print("Hello " + name + ', you are very good looking.')
+age = int(input("Also, how old are you?\n"))
+print("You are " + str(age) + " years old.")
+age_dad = int(input("Lastly, how old is your dad?\n"))
+age_diff = (age_dad - age)
+print("The age difference between you and your dad is by " + str(age_diff) + " years.")
+
+# Ask 3 questions
+# Use the stores variables somehow, print out a response
+# Can you ask questions with numerical answers?
