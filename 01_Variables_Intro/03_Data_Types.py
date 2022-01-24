22155d3b04fd6fa8faf4054a2b9347c41f61b6c3
Yacob <gfranco@hotmail.co.uk>
2022-01-24 11:48:03 +0000

    Even more examples added

--- 01_Variables_Intro/03_Data_Types.py
+++ 01_Variables_Intro/03_Data_Types.py
@@ -12,3 +12,11 @@ power = 3 ** 5  # three to the power of 5
 modulo = 3 % 5  # gives remainder after division
 
 print(13 % 3)  # 4 * 3 = 12, 13 - 12 = 1
+print(12 % 3)  # = 0
+
+print(13 / 3)  # 4.33333333333333
+print(13 // 3, 13 % 3)  # Division to a whole number = 4 to the lowest number with remainder 1
+
+third = 1 / 3
+print(third)
+print(third * 3)  # float
