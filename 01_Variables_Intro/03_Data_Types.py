87032380c1bb3009cc343ac383dd4000a6882ece
Yacob <gfranco@hotmail.co.uk>
2022-01-24 11:43:34 +0000

    Add more examples

---
+++ 01_Variables_Intro/03_Data_Types.py
@@ -0,0 +1,14 @@
+i = 375  # int
+f = 3.75  # float
+c = 3j + 2  # complex
+
+print(c, type(c))
+
+add = 3 + 5
+subtract = 3 - 5
+multiply = 3 * 5
+divide = 3 / 5
+power = 3 ** 5  # three to the power of 5
+modulo = 3 % 5  # gives remainder after division
+
+print(13 % 3)  # 4 * 3 = 12, 13 - 12 = 1
