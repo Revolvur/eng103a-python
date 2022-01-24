ae137069fb0d671028e813747edf84fc3b7a8241
Yacob <gfranco@hotmail.co.uk>
2022-01-24 10:49:41 +0000

    Variables intro

---
+++ 01_Variables_Intro/01_Variables_intro.py
@@ -0,0 +1,13 @@
+a = 1  # int
+b = 2  # int
+c = 3.5  # float (floating-point number, can be rounding errors)
+d = "Hello"  # string
+
+result = str((a + c) * b)  # calling a function
+print(d)
+print()
+
+# Python is dynamically typed, python works out what the data types are
+
+print(result, type(result))  # type gives class
+print(d, type(d))
