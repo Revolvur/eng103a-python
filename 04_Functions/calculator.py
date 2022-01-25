36f70e8d30ec22c835bc18db3702eb4650af40e6
Yacob <gfranco@hotmail.co.uk>
2022-01-25 14:34:54 +0000

    examples

---
+++ 04_Functions/calculator.py
@@ -0,0 +1,20 @@
+# The following functions
+# Each should take 2 arguments
+# And return the appropriate calculation
+
+# add
+# subtract
+# multiply
+# divide
+
+def addition(num1, num2):
+    add_ans = num1 + num2
+    print(add_ans)
+
+def subtract(num1, num2):
+    sub_ans = num1 - num2
+    print(sub_ans)
+
+
+
+addition(1,2)
