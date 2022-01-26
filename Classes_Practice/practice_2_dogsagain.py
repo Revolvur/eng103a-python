9bee415c6abc9c2b61d66366d4188b792be48570
Yacob <gfranco@hotmail.co.uk>
2022-01-26 21:45:12 +0000

    Classes exercise 3

--- Classes_Practice/practice_2_dogsagain.py
+++ Classes_Practice/practice_2_dogsagain.py
@@ -2,29 +2,35 @@ class Dog:
     # Class attribute
     species = "Canis familiaris"
 
-    def __init__(self, name, age):
+    def __init__(self, name, age):  # initialise
         self.name = name
         self.age = age
 
     def description(self):  # instance method
         return f"{self.name} is {self.age} years old."
 
-    def speak(self, sound):
+    def speak(self, sound):  # instance method
         return f"{self.name} says {sound}"
 
 
 class JackRussellTerrier(Dog):
     def speak(self, sound="Arf"):
-        return f"{self.name} says {sound}"
+        return super().speak(sound)
 
 
 class Dachshund(Dog):
-    pass
+    def speak(self, sound="Yap"):
+        return super().speak(sound)
 
 
 class Bulldog(Dog):
-    pass
+    def speak(self, sound="Woof"):
+        return super().speak(sound)
 
 
+miles = JackRussellTerrier("Miles", 4)
+buddy = Dachshund("Buddy", 9)
+jack = Bulldog("Jack", 3)
+jim = Bulldog("Jim", 5)
 miles = JackRussellTerrier("Miles", 4)
 print(miles.speak())
