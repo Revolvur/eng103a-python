e7bf05b50a62ee7a071b29ade7c95656f807b58b
Yacob <gfranco@hotmail.co.uk>
2022-01-30 18:30:51 +0000

    ...

--- Classes_Practice/practice_4_vehicle.py
+++ Classes_Practice/practice_4_vehicle.py
@@ -1,20 +1,33 @@
 class Vehicle:
 
-    def __init__(self, name, max_speed, mileage):
+    # colour = "White"
+
+    def __init__(self, name, max_speed, mileage, colour="White"):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
+        self.colour = colour
 
     def print(self):
-        print(f"Vehicle Name: {self.name} Speed: {self.max_speed} Mileage: {self.mileage}")
+        print(f"Colour: {self.colour}, Vehicle Name: {self.name}, Speed: {self.max_speed}, Mileage: {self.mileage}")
+
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity} passengers."
 
 
 class Bus(Vehicle):
     # def print(self):
     #     print(f"Vehicle Name: {self.name} Speed: {self.max_speed} Mileage: {self.mileage}")
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity)
+
+
+class Car(Vehicle):
     pass
 
+
 School = Bus("School Volvo", 180, 12)
-print(School.mileage, School.max_speed)
-print(School, type(School))
+Audi_Q5 = Car("Audi Q5", 240, 18)
 School.print()
+Audi_Q5.print()
+print(School.seating_capacity())
