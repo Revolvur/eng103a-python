21e1383705a319a7c8bfea10f78d02114ce38ebe
Yacob <gfranco@hotmail.co.uk>
2022-01-30 01:50:12 +0000

    More exercises

--- Classes_Practice/practice_4_vehicle.py
+++ Classes_Practice/practice_4_vehicle.py
@@ -1,8 +1,20 @@
 class Vehicle:
-    def __init__(self, max_speed, mileage):
-        self.maxspeed = max_speed
+
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
         self.mileage = mileage
 
+    def print(self):
+        print(f"Vehicle Name: {self.name} Speed: {self.max_speed} Mileage: {self.mileage}")
 
-pele_mobile = Vehicle(69, 69000)
-print(pele_mobile.mileage)
+
+class Bus(Vehicle):
+    # def print(self):
+    #     print(f"Vehicle Name: {self.name} Speed: {self.max_speed} Mileage: {self.mileage}")
+    pass
+
+School = Bus("School Volvo", 180, 12)
+print(School.mileage, School.max_speed)
+print(School, type(School))
+School.print()
